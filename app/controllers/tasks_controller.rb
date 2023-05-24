1fcdbe16eafc882655d65b707e7f7ad3eba65bc8
Tsuyoshi Takanashi <takenashi@me.com>
2023-05-24 20:25:54 +0900

    Dlete実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     if @task.update(params.require(:task).permit(:title, :start_date, :end_date, :all_day))
-      flash[:notice] = "ユーザーIDが「#{@task.id}」の情報を更新しました"
+      flash[:notice] = "idが「#{@task.id}」の情報を更新しました"
       redirect_to :tasks
     else
       render "edit"
@@ -38,6 +38,10 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:notice] = "予定を削除しました"
+    redirect_to :tasks
   end
 
   def task_params
